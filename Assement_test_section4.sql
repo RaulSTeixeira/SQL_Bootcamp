872fe942ba78f0b31822cd2a2c7d64410be093aa
Raul Teixeira <118553146+RaulSTeixeira@users.noreply.github.com>
2024-01-05 10:37:08 +0000

    Update Assement_test_section4.sql

--- Assement_test_section4.sql
+++ Assement_test_section4.sql
@@ -12,6 +12,7 @@
 -- The answer is Eddie Tomlin
 
 -- 1. Return the customer IDs of customers who have spent at least $110 with the staff member who has an ID of 2.
+
 Select customer_id, staff_id, sum(amount)
 From payment
 Where staff_id = 2
@@ -19,10 +20,12 @@ Group By customer_id, staff_id
 Having sum(amount)> 110;
 
 -- 2. How many films begin with the letter J?
+
 Select Count(title) from film
 Where title Like 'J%';
 
 -- 3. What customer has the highest customer ID number whose name starts with an 'E' and has an address ID lower than 500?
+
 Select first_name, customer_id, address_id, last_name from customer
 Where first_name like 'E%' and address_id < 500 
 Order By customer DESC
